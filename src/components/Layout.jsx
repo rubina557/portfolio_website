4a71a68096c1eddcd2356170cb6975504a4b1bff
Urooj Rafique <145989623+Uroojrafique5@users.noreply.github.com>
2025-07-16 05:27:15 -0700

    Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from './Navbar';
+import Footer from './Footer';
+import { useEffect, useState } from 'react';
 
-const Layout = () => {
+const Layout = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', theme === 'dark');
+  }, [theme]);
+
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50">
-      <Navbar />
-      <main className="flex-grow container mx-auto p-6">
-        <Outlet /> {/* Page content will be rendered here */}
+    <div className="bg-gradient-to-b from-[#fefaf6] to-[#eae5ff] dark:from-[#090909] dark:to-[#1a1a1a] text-gray-900 dark:text-gray-200 min-h-screen flex flex-col">
+      <Navbar theme={theme} setTheme={setTheme} />
+      <main className="flex-grow px-6 py-10 max-w-5xl mx-auto w-full">
+        {children}
       </main>
       <Footer />
     </div>
