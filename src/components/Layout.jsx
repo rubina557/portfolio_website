e324b315f14cb5af9f0cbb9ec7fe5de0a72a7353
rubina557 <rubinanazeer29@gmail.com>
2025-07-18 14:45:10 +0500

    feat:  improvements

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -17,7 +17,7 @@ const Layout = () => {
   }, [theme]);
 
   return (
-    <div className="relative min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors overflow-x-hidden">
+    <div className="relative min-h-screen flex flex-col bg-white dark:bg-[#111111] text-gray-900 dark:text-gray-100 transition-colors overflow-x-hidden">
       <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[300px] h-24 rounded-full bg-gradient-to-r from-pink-400 to-blue-400 opacity-50 blur-2xl z-0" />
 
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[300px] h-24 rounded-full bg-gradient-to-r from-pink-400 to-blue-400 opacity-50 blur-2xl z-0" />
